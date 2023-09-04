86b6554389b017fe91c29ac86be9467978c62a52
RogeMattos <rogemattos.rm@gmail.com>
2023-09-04 17:29:12 -0300

    Melhoria formulário

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -11,23 +11,22 @@ export class CarouselComponent {
 
   enviarFormulario() {
     // Obtenha os valores dos campos de entrada
-    const nome = this.el.nativeElement.querySelector('#InputForm').value;
-    const email = this.el.nativeElement.querySelector('#InputForm').value;
-    const dataNascimento = this.el.nativeElement.querySelector('#InputForm').value;
+    const nome = this.el.nativeElement.querySelector('#InputNome').value;
+    const email = this.el.nativeElement.querySelector('#InputEmail').value;
+    const dataNascimento = this.el.nativeElement.querySelector('#InputDataNascimento').value;
     const feedback = this.el.nativeElement.querySelector('#FormControlTextarea').value;
-    const nota = this.el.nativeElement.querySelector('#InputForm').value;
+    const nota = this.el.nativeElement.querySelector('#InputNota').value;
 
-  // Verifique se algum campo está vazio
+    // Verifique se algum campo está vazio
     if (!nome || !email || !dataNascimento || !feedback || !nota) {
       // Exiba um alerta se algum campo estiver vazio
       alert('Por favor, preencha todos os campos.');
-    } else if (this.validarEmail(email)) {
+    } else if (!this.validarEmail(email)) { // Note o uso do operador de negação (!) aqui
       // Verifique se o email é válido
       alert('Por favor, insira um endereço de email válido.');
     } else {
       // Se todos os campos estiverem preenchidos e o email for válido,
       // você pode executar a lógica de envio do formulário aqui
-      // Por exemplo, você pode enviar os dados para o servidor ou executar outras ações desejadas
       alert('Formulário enviado com sucesso!');
     }
   }
