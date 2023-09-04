1e3f39012a17f45309449ee21c88014e9f2808bf
RogeMattos <rogemattos.rm@gmail.com>
2023-09-04 15:30:41 -0300

    Mensagem de status de envio

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
+  
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  enviarFormulario() {
+    // Obtenha os valores dos campos de entrada
+    const nome = this.el.nativeElement.querySelector('#InputForm').value;
+    const email = this.el.nativeElement.querySelector('#InputForm').value;
+    const dataNascimento = this.el.nativeElement.querySelector('#InputForm').value;
+    const feedback = this.el.nativeElement.querySelector('#FormControlTextarea').value;
+    const nota = this.el.nativeElement.querySelector('#InputForm').value;
+
+  // Verifique se algum campo está vazio
+    if (!nome || !email || !dataNascimento || !feedback || !nota) {
+      // Exiba um alerta se algum campo estiver vazio
+      alert('Por favor, preencha todos os campos.');
+    } else if (this.validarEmail(email)) {
+      // Verifique se o email é válido
+      alert('Por favor, insira um endereço de email válido.');
+    } else {
+      // Se todos os campos estiverem preenchidos e o email for válido,
+      // você pode executar a lógica de envio do formulário aqui
+      // Por exemplo, você pode enviar os dados para o servidor ou executar outras ações desejadas
+      alert('Formulário enviado com sucesso!');
+    }
+  }
+
+  validarEmail(email: string): boolean {
+    // Expressão regular para validar um email 
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+}
+
+
+
+
+
+
+
 
   
-}
