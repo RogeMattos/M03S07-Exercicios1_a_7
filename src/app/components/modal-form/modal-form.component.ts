9c0cc09544b697d06fd67c076ded96da24d5bf5f
RogeMattos <rogemattos.rm@gmail.com>
2023-09-13 16:17:21 -0300

    Exrecício 3 - Salvar Usuário

--- src/app/components/modal-form/modal-form.component.ts
+++ src/app/components/modal-form/modal-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,13 +8,43 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './modal-form.component.html',
   styleUrls: ['./modal-form.component.css']
 })
-export class ModalFormComponent {
+export class ModalFormComponent implements OnInit {
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
 
-  constructor(private activeModal: NgbActiveModal) {}
+  usuarioForm!: FormGroup;
 
-  enviar() {}
+  constructor(private activeModal: NgbActiveModal, private http: HttpClient) {}
 
+  ngOnInit() {
+    this.criarForm();
+  }
+
+  criarForm() {
+    this.usuarioForm = new FormGroup({
+      nome: new FormControl(''),
+      email: new FormControl(''),
+      dataNascimento: new FormControl(''),
+    });
+  }
+
+  enviar() {
+    //Console.log(this.usuarioForm.value)--testa se o objeto está sendo montado corretamente--
+
+    //url do endpoint a ser chamado
+    const url = '';
+
+     //construção do objeto para envio
+    
+    const body = {
+      nomeUsuario: this.usuarioForm.value.nome,
+      email: this.usuarioForm.value.email,
+      dataNascimento: this.usuarioForm.value.dataNascimento
+    }
+    this.http.post(url, body).subscribe(retorno => {
+      //rotina
+      console.log(retorno);
+    })
+  }
   fecharModal() {
     this.closeModal?.emit();
   }
